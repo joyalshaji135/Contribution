8ea77a43fdc83746065eda16db95f8f1d6e56507
sreemayicm777 <sreemayisree123@gmail.com>
2025-06-16 20:51:21 +0530

    fileinput-changed

--- sample.js
+++ sample.js
@@ -1,9 +1,15 @@
 console.log("Hello welcome");
 
-console.log("Hello welcome");
+console.log("Hello Sreemayiiii");
 
 
 console.log("Hello welcome");
 console.log("Hello welcome");
 console.log("Hello welcome");
 console.log("Hello welcome");
+console.log("Welcome new world !!");
+console.log("Welcome new world !!");
+console.log("Welcome new world !!");
+console.log("Welcome new world !!");
+console.log("Welcome new world !!");
+console.log("Welcome new world !!");
